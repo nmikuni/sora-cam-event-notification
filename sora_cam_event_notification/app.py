72c5beb883409e6e17c3fe922b3f8a2147b55630
Naoki Mikuni <nmikuni@outlook.com>
2023-01-08 12:39:47 +0900

    prettier

--- sora_cam_event_notification/app.py
+++ sora_cam_event_notification/app.py
@@ -2,6 +2,7 @@ import datetime
 import os
 
 import requests
+
 import line_notify
 import soracom_cli
 
@@ -19,18 +20,23 @@ EVENT_RETRIEVE_INTERVAL_SEC = int(
 def lambda_handler(event, context):
     print("Get the motion events list with SORACOM API")
     motion_events = soracom_cli.list_events(
-        device_id=DEVICE_ID, delta_sec=EVENT_RETRIEVE_INTERVAL_SEC, arg=COMMON_ARG)
+        device_id=DEVICE_ID,
+        event_retrieve_interval_sec=EVENT_RETRIEVE_INTERVAL_SEC,
+        arg=COMMON_ARG)
     if motion_events == []:
         print("There was no event.")
         return
     for motion_event in motion_events:
-        event_detected_time = motion_event["time"]
+        event_detected_epoch_ms = motion_event["time"]
         image_url = motion_event["eventInfo"]["atomEventV1"]["picture"]
         image_bytes = download_image(image_url)
-        notify_message = create_event_detection_message(event_detected_time)
+        message_text = create_event_detection_message(
+            event_detected_epoch_ms)
         print("Notify the image")
         line_notify.notify_to_line_with_image(
-            token=LINE_NOTIFY_TOKEN, message=notify_message, image_bytes=image_bytes)
+            token=LINE_NOTIFY_TOKEN,
+            message=message_text,
+            image_bytes=image_bytes)
     return
 
 
@@ -39,10 +45,10 @@ def download_image(image_url):
     return image_data_bytes
 
 
-def create_event_detection_message(event_detected_time):
-    event_detected_time_dt = datetime.datetime.utcfromtimestamp(
-        int(event_detected_time) / 1000)
-    event_detected_time_iso = event_detected_time_dt.isoformat() + 'Z'
+def create_event_detection_message(event_detected_epoch_ms):
+    event_detected_dt = datetime.datetime.utcfromtimestamp(
+        int(event_detected_epoch_ms) / 1000)
+    event_detected_time_iso = event_detected_dt.isoformat() + 'Z'
 
     message = 'There was an event at ' + event_detected_time_iso
 
--- sora_cam_event_notification/soracom_cli.py
+++ sora_cam_event_notification/soracom_cli.py
@@ -10,11 +10,17 @@ def run_cmd(cmd, arg):
 
 
 def list_events(device_id, event_retrieve_interval_sec, arg):
+    """
+    Get the list of motion detection events from Soracom Cloud Camera Service
+    The program uses soracom-cli.
+    If the code run in Lambda, the soracom-cli should be used by Lambda layer.
+    """
     current_unix_time_ms = int(time.time() * 1000)
     delta_ms = event_retrieve_interval_sec * 1000
     from_unix_time_ms = current_unix_time_ms - delta_ms
-    list_sora_cam_events_cmd = "soracom sora-cam devices events list-for-device --device-id " + \
-        device_id + " --from " + str(from_unix_time_ms)
+    list_sora_cam_events_cmd = "soracom sora-cam devices events " \
+        + "list-for-device --device-id " + device_id + \
+        " --from " + str(from_unix_time_ms)
     event_list = json.loads(
         run_cmd(cmd=list_sora_cam_events_cmd, arg=arg).stdout.decode())
     return event_list
